d209316bcd83ffc0475e9444c5b2ab1107ab996c
beamedina <alexandra.medina@estudante.ifro.edu.br>
2025-05-14 15:20:07 -0400

    14/05

--- servico/conexao.js
+++ servico/conexao.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     host: 'ss04ggwskkwc0ksgcookg48w',
     port: '3306',
-    user: 'IFtness',
+    user: 'iffitness',
     password: '12345678',
     database: 'iffitness_db'
     })
