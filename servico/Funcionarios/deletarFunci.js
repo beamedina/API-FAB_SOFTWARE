21be2c984205f88420d66128c34fb0b0aed7baae
beamedina <alexandra.medina@estudante.ifro.edu.br>
2025-05-15 10:33:26 -0400

    aaaa

--- servico/Funcionarios/deletarFunci.js
+++ servico/Funcionarios/deletarFunci.js
@@ -3,7 +3,7 @@ import pool from "../conexao.js"
 export async function DeletaFunci(id) {
     const conexao = await pool.getConnection();
 
-    const query = 'DELETE from funcionario WHERE idFUNCIONARIO = ?';
+    const query = 'DELETE from FUNCIONARIO WHERE idFUNCIONARIO = ?';
     const [resposta] = await conexao.execute(query, [id]);
     console.log(resposta);
     conexao.release();
