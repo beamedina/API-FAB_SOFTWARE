eda8096914c837ca07ecc3d91c233bc9a836fda9
beamedina <alexandra.medina@estudante.ifro.edu.br>
2025-05-18 20:56:24 -0400

    18/05

--- servico/Funcionarios/login.js
+++ servico/Funcionarios/login.js
@@ -2,7 +2,7 @@ import pool from "../conexao.js";
 export async function verificarLogin(email, senha) {
     const conexao = await pool.getConnection();
     const [results] = await conexao.query(
-      'SELECT * FROM funcionario WHERE email = ? AND senha = ?',
+      'SELECT * FROM FUNCIONARIO WHERE email = ? AND senha = ?',
       [email, senha]
     );
   
